dca975c89eb8c79694a5476ef58801ce6d33c3f9
EmelyFuentes <00126025@uca.edu.sv>
2025-06-07 15:52:47 -0600

    aaa

--- arrays.cpp
+++ arrays.cpp
@@ -1,69 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-int numero[20], buscar, i =  0;
+int main()
+{
+    int numero[20], buscar;
 
-cout<<"Ingrese un numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
-cout<<"Ingrese otro numero "<<endl;
-cin>>numero[20];
+    for (int i = 0; i < 20; i++)
+    {
+        cout << "Ingrese un numero " <<endl;
+        cin >> numero[i];
+    }
 
-    cout<<"Ingrese un numero para buscar "<<endl;
-    cin>>buscar;
-    i--;
-    if ((numero[20] == buscar)||(buscar > 1 && buscar < 20))
+    cout << "Ingrese un numero para buscar " <<endl;
+    cin >> buscar;
+
+    for (int i = 0; i < 0; i++)
+    {
+        /* code */
+    }
+    
+
+    if ((numero[20] == buscar) || (buscar > 1 && buscar < 20))
 
         for (int i = numero[20]; i <= 20; i++)
         {
-    {
-    cout<<"El numero que ingreso si esta en los numeros digitados antes: "<<buscar<<endl;
-    }
-    }   
+            {
+                cout << "El numero que ingreso si esta en los numeros digitados antes: " << buscar << endl;
+            }
+        }
     else if (buscar != 0)
     {
-    cout<<"El numero que ingreso no esta en los numeros digitados antes: "<<buscar<<endl;
+        cout << "El numero que ingreso no esta en los numeros digitados antes: " << buscar << endl;
     }
     else
     {
-    cout<<"El numero que ingreso es 0: "<<buscar<<endl;
+        cout << "El numero que ingreso es 0: " << buscar << endl;
     }
-
     return 0;
 }
--- notas.cpp
+++ notas.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main(){
-int codigo, notas, promedio;
-string estudiante;
+int carnet, devoler, prestamo, salida;
+string nombre;
+
+cout<<"Digite nombre de usuario"<<endl;
+cin>>nombre;
+cout<<"Digite numero de carnet"<<endl;
+cin>>carnet;
+
+if (carnet)
+{
+    (carnet = 8 && carnet > 1);
+    cout<<"Su numero de carnet se guardo correctamente"<<endl;
+}
+else
+{
+cout<<"Su numero de carnet no es valido"<<endl;
+}
+
+cout<<"1. Prestar libro"<<endl;
+cout<<"1. Devolver libro"<<endl;
+cout<<"1. Salir"<<endl;
+
 
     return 0;
 }
